95ee52e10cca6672477237781773ded88ccb3681
vaishnavw45 <vaishnavc2445@gmail.com>
2025-02-09 21:58:04 +0530

    module added

---
+++ src/Components/Completed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Completed() {
+  return (
+    <div>
+      function complete() {
+        <h1>completed</h1>
+      }
+    </div>
+  )
+}
+
+export default Completed
